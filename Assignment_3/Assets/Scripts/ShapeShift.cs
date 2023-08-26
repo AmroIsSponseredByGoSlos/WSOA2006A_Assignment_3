5c7a60be08db1bfed003272ad0bcff83cebc7259
Amro <2592997@students.wits.ac.za>
2023-08-27 01:13:14 +0200

    Selectable Props Reset

--- Assignment_3/Assets/Scripts/ShapeShift.cs
+++ Assignment_3/Assets/Scripts/ShapeShift.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class ShapeShift : MonoBehaviour
 {
     public float RayLength = 10f;
+    public Material HighLight;
+    public Material NormalMaterial;
+    public GameObject Props;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,10 +21,19 @@ public class ShapeShift : MonoBehaviour
         if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out Hit, RayLength))
         {
             Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * RayLength, Color.white);
-            Debug.Log(Hit.transform.gameObject.name);
             if (Hit.transform.gameObject.CompareTag("Selectable"))
             {
-                Debug.Log("Can change into this");
+                Renderer ObjectRenderer = Hit.transform.gameObject.GetComponent<Renderer>();
+                ObjectRenderer.material = HighLight;
+            }
+        }
+        else
+        {
+            for (int i = 0; i < Props.transform.childCount; i++)
+            {
+                Transform child = Props.transform.GetChild(i);
+                Renderer ChildRenderer = child.GetComponent<Renderer>();
+                ChildRenderer.material = NormalMaterial;
             }
         }
     }
